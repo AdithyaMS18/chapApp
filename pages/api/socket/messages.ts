8646b4ddbd8ba466d64d5e442452d0d463116a61
WeirdFingers <107349258+WeirdFingers@users.noreply.github.com>
2024-04-09 08:25:18 +0530

    message

--- pages/api/socket/messages.ts
+++ pages/api/socket/messages.ts
@@ -1,33 +1,35 @@
 import { NextApiRequest } from "next";
-import { MemberRole } from "@prisma/client";
-
 import { NextApiResponseServerIo } from "@/types";
 import { currentProfilePages } from "@/lib/current-profile-pages";
+
 import { db } from "@/lib/db";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponseServerIo,
 ) {
-  if (req.method !== "DELETE" && req.method !== "PATCH") {
-    return res.status(405).json({ error: "Method not allowed" });
+  if (req.method !== "POST"){
+    return res.status(405).json({error: "Method not allowed"});
   }
-
-  try {
+  try{
     const profile = await currentProfilePages(req);
-    const { messageId, serverId, channelId } = req.query;
-    const { content } = req.body;
+    const { content, fileUrl } = req.body;
+    const { serverId, channelId } = req.query;
 
-    if (!profile) {
+    if(!profile){
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    if (!serverId) {
+    if(!serverId){
       return res.status(400).json({ error: "Server ID missing" });
     }
 
-    if (!channelId) {
+    if(!channelId){
       return res.status(400).json({ error: "Channel ID missing" });
+    }
+
+    if(!content){
+      return res.status(400).json({ error: "Content missing" });
     }
 
     const server = await db.server.findFirst({
@@ -35,112 +37,58 @@ export default async function handler(
         id: serverId as string,
         members: {
           some: {
-            profileId: profile.id,
+            profileId: profile.id
           }
         }
       },
       include: {
         members: true,
       }
-    })
+    });
 
-    if (!server) {
-      return res.status(404).json({ error: "Server not found" });
+    if(!server) {
+      return res.status(404).json({ message: "Server not found"});
     }
 
     const channel = await db.channel.findFirst({
       where: {
         id: channelId as string,
         serverId: serverId as string,
-      },
+      }
     });
-  
-    if (!channel) {
-      return res.status(404).json({ error: "Channel not found" });
+
+    if(!channel) {
+      return res.status(404).json({ message: "Channel not found"});
     }
 
     const member = server.members.find((member) => member.profileId === profile.id);
 
-    if (!member) {
-      return res.status(404).json({ error: "Member not found" });
+    if(!member) {
+      return res.status(404).json({ message: "Member not found"});
     }
 
-    let message = await db.message.findFirst({
-      where: {
-        id: messageId as string,
+    const message = await db.message.create({
+      data: {
+        content,
+        fileUrl,
         channelId: channelId as string,
-      },
-      include: {
-        member: {
-          include: {
-            profile: true,
-          }
+        memberId: member.id,
+    },
+    include: {
+      member:{
+        include: {
+          profile: true,
         }
       }
-    })
-
-    if (!message || message.deleted) {
-      return res.status(404).json({ error: "Message not found" });
     }
+    });
+    const channelKey = `chat:${channelId}:messages`;
 
-    const isMessageOwner = message.memberId === member.id;
-    const isAdmin = member.role === MemberRole.ADMIN;
-    const isModerator = member.role === MemberRole.MODERATOR;
-    const canModify = isMessageOwner || isAdmin || isModerator;
-
-    if (!canModify) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    if (req.method === "DELETE") {
-      message = await db.message.update({
-        where: {
-          id: messageId as string,
-        },
-        data: {
-          fileUrl: null,
-          content: "This message has been deleted.",
-          deleted: true,
-        },
-        include: {
-          member: {
-            include: {
-              profile: true,
-            }
-          }
-        }
-      })
-    }
-
-    if (req.method === "PATCH") {
-      if (!isMessageOwner) {
-        return res.status(401).json({ error: "Unauthorized" });
-      }
-
-      message = await db.message.update({
-        where: {
-          id: messageId as string,
-        },
-        data: {
-          content,
-        },
-        include: {
-          member: {
-            include: {
-              profile: true,
-            }
-          }
-        }
-      })
-    }
-
-    const updateKey = `chat:${channelId}:messages:update`;
-
-    res?.socket?.server?.io?.emit(updateKey, message);
+    res?.socket?.server?.io?.emit(channelKey, message);
 
     return res.status(200).json(message);
   } catch (error) {
-    console.log("[MESSAGE_ID]", error);
-    return res.status(500).json({ error: "Internal Error" });
+    console.log("[MESSAGES_POST]",error);
+    return res.status(500).json({ message: "Internal Error"});
   }
 }
